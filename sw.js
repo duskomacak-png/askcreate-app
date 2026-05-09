0a66351701079b00faf79de32e4bf4a576b98926
duskomacak-png <duskomacak@gmail.com>
2026-05-09 15:31:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "startwork-pro-v1280";
+const CACHE_NAME = "startwork-pro-v1281";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=1280",
-  "./script.js?v=1280",
+  "./style.css?v=1281",
+  "./script.js?v=1281",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
