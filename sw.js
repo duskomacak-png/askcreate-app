815229676f3bd681c8a1d69b643fb819435c6fa7
duskomacak-png <duskomacak@gmail.com>
2026-05-24 11:49:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "askcreate-app-v1322";
+const CACHE_NAME = "askcreate-app-v1323";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=1322",
-  "./script.js?v=1322",
+  "./style.css?v=1323",
+  "./script.js?v=1323",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
